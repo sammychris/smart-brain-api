2cc4a8a5ea03bc31bc638abb3ee024d52c0965a3
sammychris <ebusameric@gmail.com>
2022-03-04 18:10:10 +0100

    feat: Add morgan for logs and pg configurations

--- server.js
+++ server.js
@@ -1,37 +1,54 @@
-const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bodyParser = require("body-parser"); // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const morgan = require("morgan");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
   // connect to your own database here:
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+  client: "pg",
+  connection: process.env.DATABASE_URI,
+  useNullAsDefault: true,
 });
+
+db.raw("SELECT 1")
+  .then((a) => {
+    console.log("PostgreSQL connected");
+  })
+  .catch((e) => {
+    console.log("PostgreSQL not connected");
+    console.error(e);
+  });
 
 const app = express();
 
-app.use(cors())
+app.use(morgan("combined"));
+app.use(cors());
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get("/", (req, res) => {
+  res.send(db.users);
+});
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
